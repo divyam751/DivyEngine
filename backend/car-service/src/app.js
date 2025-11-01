3b9e9e2bc706b079ef26f9893a5cd8bd34a61a60
Divyam Chauhan <mi.divyam@gmail.com>
2025-11-01 09:26:43 +0530

    update : add health check route

--- backend/car-service/src/app.js
+++ backend/car-service/src/app.js
@@ -8,7 +8,15 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World! Car Service is running.");
+  res
+    .status(200)
+    .json({ status: "success", message: "Welcome to the Car Service" });
+});
+
+app.get("/health", (req, res) => {
+  res
+    .status(200)
+    .json({ status: "success", message: "Car Service is healthy" });
 });
 
 app.use("/api/v1/car", carRouter);
